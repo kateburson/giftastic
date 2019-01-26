0f228ac011c89452a59a3b5b8b463c3fdbfd530a
Edward Hopper <burson.kate@gmail.com>
2019-01-26 15:02:01 -0800

    animate/still working

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -9,20 +9,24 @@ $(document).ready(function() {
 
 
     function newButtons() {
-
         event.preventDefault()
-
-        var input = $("#verb-input").val();
+        var input = $('#verb-input').val();
         verbs.push(input);
-        $('.button-farm').append('<button class="new-verb" value="' + input + '">' + input + '</button>');
-        
-    }; //newButtons
+        $('.button-farm').append('<button class="searchbutton" value="' + input + '">' + input + '</button>');
 
-    $('#add-verb').on('click', newButtons);
+        $('#add-verb').on('click', function(){
+            newButtons();
+            $('#verb-input').val('');
+    
+        });
+
+        renderButtons();  
+
+
+    }; //newButtons
 
     function renderButtons() {
         $('.button-farm').html('');
-
         for (var i = 0; i < verbs.length; i++) {
             var button = $('<button>');
             button.text(verbs[i]);
@@ -55,20 +59,35 @@ $(document).ready(function() {
         }).then(function(response){
             console.log(queryURL);
             console.log(response);
-            for(var i = 0; i < queryURL.length; i++) {
+            for(var i = 0; i < response.data.length; i++) {
                 var div = $('<div class="image-container">');
                 var p = $('<p>');
                 var img = $('<img>');
-                var imgURL = response.data[i].images.downsized.url;
-                img.attr({src: imgURL, width: '300px', height: 'auto'});
+                img.attr('class', 'gif');
+                var source = response.data[i].images.downsized.url;
+                var still = response.data[i].images.downsized_still.url;
+                img.attr({src: still, 'data-still': still, 'data-animate': source, 'data-state' : 'still' , width: '300px', height: 'auto'});
                 $(div).append(img);
-                $(p).append(response.data[i].rating);
+                $(p).append(response.data[i].rating.toUpperCase());
                 $(div).append(p);
                 $('#verb-view').append(div);
             } 
-        }); 
-        
 
+            $('.gif').on("click", function() {
+                console.log('clicked');
+                var state = $(this).attr('data-state');
+                console.log(state);
+                if(state === 'still'){
+                    $(this).attr('src',$(this).attr('data-animate'));
+                    $(this).attr('data-state','animate');
+                } else {
+                    $(this).attr('src',$(this).attr('data-still'));
+                    $(this).attr('data-state','still');
+                } 
+        
+            });
+
+        }); 
 
     }; // doSearch
 
