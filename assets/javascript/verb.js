f86041b8a13c0af080ee39f4289b10cb8f22beba
Edward Hopper <burson.kate@gmail.com>
2019-01-27 21:16:11 -0800

    favorites section almost done

--- assets/javascript/verb.js
+++ assets/javascript/verb.js
@@ -9,6 +9,32 @@ $(document).ready(function() {
         'roll',
     ];
 
+    var favoriteGifs = [];
+
+    function animate() {
+        var state = $(this).attr('data-state');
+        if(state === 'still'){
+            $(this).attr('src',$(this).attr('data-animate'));
+            $(this).attr('data-state','animate');
+        } else {
+            $(this).attr('src',$(this).attr('data-still'));
+            $(this).attr('data-state','still');
+        } 
+    };
+
+    function favorite() {
+        var state = $(this).attr('class');
+        var favGif = $(this).parent().find($('<img>'));
+        if(state === 'far fa-heart small') {
+            $(this).attr('class','fas fa-heart small');
+            favoriteGifs.push(favGif);
+            console.log('gif added to favorites');
+        } else if(state === 'fas fa-heart small') {
+            $(this).attr('class','far fa-heart small');
+            favoriteGifs.splice(favoriteGifs.indexOf(favGif), 1);
+        }
+        console.log(favoriteGifs);
+    }
 
     function newButtons() {
         event.preventDefault()
@@ -77,35 +103,15 @@ $(document).ready(function() {
                 $('#verb-view').append(div);
             } 
 
-            $('.gif').on("click", function() {
-                console.log('clicked');
-                var state = $(this).attr('data-state');
-                console.log(state);
-                if(state === 'still'){
-                    $(this).attr('src',$(this).attr('data-animate'));
-                    $(this).attr('data-state','animate');
-                } else {
-                    $(this).attr('src',$(this).attr('data-still'));
-                    $(this).attr('data-state','still');
-                } 
-            });
+            $('.gif').on("click", animate);
 
-            $('.fa-heart').on('click', function() {
-                var state = $(this).attr('class');
-                // var gif = $(this).
-                console.log('heart clicked', state);
-                if(state === 'far fa-heart small') {
-                    $(this).attr('class','fas fa-heart small');
-                } else if(state === 'fas fa-heart small') {
-                    $(this).attr('class','far fa-heart small');
-                }
-            });
+            $('.fa-heart').on('click', favorite);
+            
+            $('#verb-input').focus();
 
         }); 
 
     }; // doSearch
-
-    var favorites = [];
      
     $('.favorites').on('click', function() {
         $('#verb-view').empty();
@@ -113,9 +119,21 @@ $(document).ready(function() {
         console.log('favorites heart clicked', state);
         if(state === 'far fa-heart favorites') {
             $(this).attr('class','fas fa-heart favorites');
+            for(var i = 0; i < favoriteGifs.length; i++) {
+                var div = $('<div class="image-container">');
+                var info = favoriteGifs[i].prevObject[0].innerHTML;
+                console.log(info);
+                $(div).append(info);
+                $('#verb-view').append(div);
+            }
         } else if (state === 'fas fa-heart favorites'){
             $(this).attr('class','far fa-heart favorites');
         }
+
+        $('.gif').on("click", animate);
+
+        $('.fa-heart').on('click', favorite);
+
     });
 
     renderButtons();
